e4db09f56285c698cd007be38014535734b9ee65
Akamarai <simone.fabber@gmail.com>
2022-12-04 10:28:52 +0100

    Big Update for Gnome43

--- ArchSetup.sh
+++ ArchSetup.sh
@@ -12,23 +12,9 @@
 
 #Gnome extension  and shortcut (attached dconf file)
 	#These two lines used to export the config
-	#dconf dump '/org/gnome/desktop/wm/keybindings/' > keybindings.dconf
-	#dconf dump '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' > custom-keybindings.dconf
+	#dconf dump /org/gnome/desktop/wm/keybindings/ > keybindings.dconf
+	#dconf dump /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ > custom-keybindings.dconf
 	
-	#List of extensions
-	# user-theme@gnome-shell-extensions.gcampax.github.com
-	# sound-output-device-chooser@kgshank.net
-	# clipboard-indicator@tudmotu.com
-	# Vitals@CoreCoding.com
-	# AlphabeticalAppGrid@stuarthayhurst
-	# app-grid-tweaks@Selenium-H
-	# blur-my-shell@aunetx
-	# floating-dock@nandoferreira_prof@hotmail.com
-	# gnome-ui-tune@itstime.tech
-	# nightthemeswitcher@romainvigier.fr
-	# pop-shell@system76.com
-
-
 ############
 #  SCRIPT  #
 ############
@@ -38,7 +24,7 @@ bypass() {
   while true;
   do
     sudo -n true
-    sleep 500
+    sleep 1000
     kill -0 "$$" || exit
   done 2>/dev/null &
 }
@@ -46,38 +32,28 @@ bypass() {
 echo "Starting Arch Linux post-install script..."
 	sleep 2s
 		bypass
-
+	
 	clear
 
 
 echo "Updating system..."
 	sleep 2s
 		sudo pacman -Syu
-
+	
 	clear
 
 
 echo "Installing packages..."
 #Steam pkg need [multilib] repository uncommented in /etc/pacman.conf
-#for pure Arch add os-prober firefox
 	sleep 2s
-		sudo sed -i 's/ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
-		sudo pacman -S linux-zen linux-zen-headers pigz pbzip2 baobab micro s-tui htop \
-		wl-clipboard code gnome-boxes chromium piper vlc bat fragments \
-		telegram-desktop papirus-icon-theme helvum guitarix steam discord \
-		vlc
-		sudo grub-mkconfig -o /boot/grub/grub.cfg
+	
+		sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
+		sudo pacman -S bind bc micro git firefox papirus-icon-theme wl-clipboard bash-completion ttf-roboto ttf-ubuntu-font-family \
+			ttf-jetbrains-mono alacritty gnome-terminal neofetch pigz pbzip gnome-boxes linux-zen linux-zen-headers linux-headers piper \
+			gufw ibva-mesa-driver mesa-vdpau power-profiles-daemon powertop htop s-tui os-prober reflector 
+	
 	clear
-
-#echo "Installing AUR helper"
-#	sleep 2s
-#		cd
-#		sudo pacman -S --needed base-devel
-#		cd ~/GitHub && git clone https://aur.archlinux.org/paru.git && cd paru
-#		makepkg -si
-#
-#	clear		
-
+	
 echo "Tuning system..."
 #USARE : sed -i 's/riga da canellare/riga da aggiungere/' path/to/file
 	sleep 2s
@@ -88,45 +64,79 @@ echo "Tuning system..."
 		sudo sed -i 's/COMPRESSBZ2=(bzip2 -c -f)/COMPRESSBZ2=(pbzip2 -c -f)/' /etc/makepkg.conf
 		sudo sed -i 's/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q --threads=0 -)/' /etc/makepkg.conf
 	#pacman
-		#sudo sed -i 's/#Color/Color/' /etc/pacman.conf
-	#paru
-		#sudo sed -i "/BottomUp/s/^#//g" /etc/paru.conf
+		sudo sed -i 's/#Color/Color/' /etc/pacman.conf
+		sed -i '33 i ILoveCandy' /etc/pacman.conf
 	# #grub
-	 	#echo "" | sudo tee -a /etc/default/grub
-	 	#echo "#For Windows" | sudo tee -a /etc/default/grub
-	 	#echo "GRUB_DISABLE_OS_PROBER=false" | sudo tee -a /etc/default/grub
-	 	#sudo grub-mkconfig -o /boot/grub/grub.cfg	
+	 	sudo sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
+	 	sudo grub-mkconfig -o /boot/grub/grub.cfg	
 	#/etc/env
-	#sudo sed -i 's/EDITOR=nano/	EDITOR=micro/' /etc/environment
-	#echo "MOZ_ENABLE_WAYLAND=1" | sudo tee -a /etc/environment
-	clear
+	echo "MOZ_ENABLE_WAYLAND=1" | sudo tee -a /etc/environment
 	
+	clear
 
-echo "Installing GitHub Theme Files..."
-	sleep 1s
-		cd ~/GitHub && git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git && cd WhiteSur-gtk-theme
-		./install.sh -t red -i arch -N glassy -HD 
-		sudo ./tweaks.sh -g -c dark -t red
+echo "Installing AUR helper"
+	sleep 2s
+		cd
+		mkdir GitHub
+		#For paru
+		#sudo pacman -S --needed base-devel
+		# cd ~/GitHub && git clone https://aur.archlinux.org/paru.git && cd paru
+		# makepkg -si
+		#sudo sed -i "/BottomUp/s/^#//g" /etc/paru.conf
 		
-	sleep 1s
-		cd ~/GitHub && git clone https://github.com/vinceliuice/Vimix-cursors.git && cd Vimix-cursors
-		sudo ./install.sh
-		
-	clear
-	
+		#For yay
+		sudo pacman -S --needed base-devel
+		cd ~/GitHub && git clone https://aur.archlinux.org/yay.git && cd yay
+		makepkg -si
+		sleep 1s
+		yay -Y --devel --combinedupgrade --batchinstall --save
+
+	clear	
 
 echo "Installing AUR pkg..."
-#EndevourOS has default yay. Uncomment Paru line and change yay with paru if u want it
 	sleep 2s
-		yay -S topgrade spotify droidcam minecraft-launcher \
-		zoom gnome-shell-extension-pop-shell-git marktext-bin \
-		onlyoffice papirus-folders-git gnome-text-editor \
-		whatsdesk-bin bash-pipes noisetorch timeshift chrome-gnome-shell-git \
-		nerd-fonts-complete popsicle
+		yay -S spotify minecraft-launcher zoom gnome-shell-extension-pop-shell-git marktext-bin \
+		onlyoffice papirus-folders-git whatsdesk-bin bash-pipes noisetorch gnome-browser-connector-git \
+		visual-studio-code-bin google-chrome catppuccin-gtk-theme-mocha catppuccin-gtk-theme-latte \
+		extension-manager
 		
 		sleep 1s 
-		papirus-folders -C yaru
+		papirus-folders -C pink
 
 	clear
+
+echo "Systemd Service"
+	sleep 2s
+		systemctl enable ufw.service
+		systemctl enable power-profiles-daemon.service
+		
+	clear
+
+echo "Tune Electron App"
+	sleep 2s
+		cd
+		cp ~/GitHub/ArchStuff/code-flags.conf .config/
+		cp ~/GitHub/ArchStuff/chrome-flags.conf .config/
+		sudo cp ~/GitHub/ArchStuff/marktext.desktop usr/share/applications/marktext.desktop
+		sudo cp ~/GitHub/ArchStuff/marktext.desktop usr/share/applications/heroic.desktop
+
+	clear
+
+echo "Huawei Settings"
+	sleep 2s
+		yay -S matebook-applet huawei-wmi
+		systemctl enable huawei-wmi-reinstate.service
+		systemctl enable huawei-wmi-privilege.service
+		sudo usermod -a -G huawei-wmi $USER
+	
+	clear
+
+echo "Copy Dots"
+	sleep 2s
+		cd
+		mkdir .config/alacritty
+		cp ~/GitHub/ArchStuff/alacritty.yml .config/alacritty/alacritty.yml
+		
+
 read -p "Done! See script comment to manual install gnome extension and shortcut. Press ENTER to reboot."
 	reboot
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # ArchStuff
+
 Simple bash script to tune a few things after a fresh arch installation. Based on EndeavourOS
 Here two line to execute it:
+
 ```bash
 cd $HOME 
 mkdir GitHub && cd GitHub
@@ -9,21 +11,44 @@ chmod +x ArchSetup.sh
 ./ArchSetup.sh
 ```
 
-
 ##### Preview of Day-theme
-![Screenshot from 2022-02-04 10-34-01](https://user-images.githubusercontent.com/87226280/152510107-69fa05da-cef3-41b8-ae6f-b3a14cb508d0.png)
+
+
 
 ##### Preview of Night-theme
-![Screenshot from 2022-02-04 10-34-20](https://user-images.githubusercontent.com/87226280/152510144-5e2040b0-1b62-433f-8f46-5ec6d596a205.png)
 
 #### After manually install the extension use this two line to load my shortcut and settings
+
+> Alphabetical App Grid
+> 
+> AppIndicator and KstatusNotifierltem Support
+> 
+> Aylur's Widgets
+> 
+> Blur my Shell
+> 
+> Dash to Dock
+> 
+> Gnome 4x UI Improvements
+> 
+> Just Perfections
+> 
+> Night Theme Switcher
+> 
+> Rounded Windows Corners
+> 
+> Vitals
+> 
+> User Themes
+
 ```bash
 dconf load /org/gnome/shell/extensions/ < /$HOME/GitHub/ArchStuff/extension-settings.dconf
 dconf load /org/gnome/desktop/wm/keybindings/ < /$HOME/GitHub/ArchStuff/keybindings.dconf
-dconf load /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ < /$HOME/GitHub/ArchStuff/custom-keybindings.dconf
+dconf load /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ < /$HOME/GitHub/ArchStuff/custom-keybindings.dconf
 ```
 
 ##### To be Fixed
+
 In NightThemeSwitcher extension not able to add general path like  `file:///$HOME/GitHub/Pictures/89206202_p0.jpg` to set background, so you have to modify like this: `file:///home/(your username)/GitHub/Pictures/89206202_p0.jpg`
 
 ##### Wallpaper Credit :
