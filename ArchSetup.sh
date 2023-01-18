78b9d05fa7db33462e722e88a5976132d957c828
Akamarai <87226280+Akamarai@users.noreply.github.com>
2023-01-18 13:35:59 +0100

    Update and Fix

--- ArchSetup.sh
+++ ArchSetup.sh
@@ -41,7 +41,7 @@ echo "Installing packages..."
 #Steam pkg need [multilib] repository uncommented in /etc/pacman.conf
 	sleep 2s
 		sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
-		sudo pacman -S bat micro git firefox papirus-icon-theme wl-clipboard bash-completion ttf-roboto ttf-ubuntu-font-family ttf-jetbrains-mono alacritty neofetch pigz lbzip2  gnome-boxes linux-zen linux-zen-headers linux-headers piper gufw ibva-mesa-driver mesa-vdpau powertop htop s-tui os-prober reflector obsidian 
+		sudo pacman -S bat micro git firefox papirus-icon-theme wl-clipboard bash-completion ttf-roboto ttf-ubuntu-font-family ttf-jetbrains-mono alacritty neofetch pigz lbzip2  gnome-boxes linux-zen linux-zen-headers linux-headers piper gufw ibva-mesa-driver mesa-vdpau powertop htop s-tui os-prober reflector obsidian steam discord os-prober ntfs-3g
 	
 echo "Tuning system..."
 #USARE : sed -i 's/riga da canellare/riga da aggiungere/' path/to/file
@@ -60,6 +60,12 @@ echo "Tuning system..."
 	 	sudo grub-mkconfig -o /boot/grub/grub.cfg	
 	#/etc/env
 	echo "MOZ_ENABLE_WAYLAND=1" | sudo tee -a /etc/environment
+	echo "EDITOR=micro" | sudo tee -a /etc/environment
+	echo "BROWSER=firefox" | sudo tee -a /etc/environment
+	echo "TERMINAL=alacritty" | sudo tee -a /etc/environment
+	echo "QT_QPA_PLATFORM=wayland" | sudo tee -a /etc/environment
+	
+	
 
 echo "Installing AUR helper"
 	sleep 2s
@@ -81,7 +87,7 @@ echo "Installing AUR helper"
 
 echo "Installing AUR pkg..."
 	sleep 2s
-		paru -S prismlauncher zoom gnome-shell-extension-pop-shell-git papirus-folders-git visual-studio-code-bin google-chrome extension-manager github-desktop-bin
+		paru -S prismlauncher zoom gnome-shell-extension-pop-shell-git papirus-folders-git visual-studio-code-bin google-chrome extension-manager github-desktop-bin heroic-games-launcher-bin auto-cpufreq
 		
 		sleep 1s 
 		papirus-folders -C pink
@@ -89,15 +95,15 @@ echo "Installing AUR pkg..."
 echo "Systemd Service"
 	sleep 2s
 		systemctl enable ufw.service
-		systemctl enable power-profiles-daemon.service
+		systemctl enable auto-cpufreq.service
 
 echo "Tune Electron App"
 	sleep 2s
 		cd
 		cp ~/GitHub/ArchStuff/code-flags.conf .config/
 		cp ~/GitHub/ArchStuff/chrome-flags.conf .config/
-		sudo cp ~/GitHub/ArchStuff/marktext.desktop usr/share/applications/marktext.desktop
-		sudo cp ~/GitHub/ArchStuff/marktext.desktop usr/share/applications/heroic.desktop
+		sudo cp ~/GitHub/ArchStuff/heroic.desktop usr/share/applications/heroic.desktop
+
 
 echo "Huawei Settings"
 	sleep 2s
