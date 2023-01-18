f82fe94b89fcd36585952aec0d20f0ad095b2b74
Akamarai <87226280+Akamarai@users.noreply.github.com>
2023-01-18 13:27:23 +0100

    Pulizia e fix

--- ArchSetup.sh
+++ ArchSetup.sh
@@ -32,27 +32,16 @@ bypass() {
 echo "Starting Arch Linux post-install script..."
 	sleep 2s
 		bypass
-	
-	clear
-
 
 echo "Updating system..."
 	sleep 2s
 		sudo pacman -Syu
-	
-	clear
-
 
 echo "Installing packages..."
 #Steam pkg need [multilib] repository uncommented in /etc/pacman.conf
 	sleep 2s
-	
 		sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
-		sudo pacman -S bind bc micro git firefox papirus-icon-theme wl-clipboard bash-completion ttf-roboto ttf-ubuntu-font-family \
-			ttf-jetbrains-mono alacritty gnome-terminal neofetch pigz pbzip gnome-boxes linux-zen linux-zen-headers linux-headers piper \
-			gufw ibva-mesa-driver mesa-vdpau power-profiles-daemon powertop htop s-tui os-prober reflector 
-	
-	clear
+		sudo pacman -S bat micro git firefox papirus-icon-theme wl-clipboard bash-completion ttf-roboto ttf-ubuntu-font-family ttf-jetbrains-mono alacritty neofetch pigz lbzip2  gnome-boxes linux-zen linux-zen-headers linux-headers piper gufw ibva-mesa-driver mesa-vdpau powertop htop s-tui os-prober reflector obsidian 
 	
 echo "Tuning system..."
 #USARE : sed -i 's/riga da canellare/riga da aggiungere/' path/to/file
@@ -61,7 +50,7 @@ echo "Tuning system..."
 		sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
 		sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z --threads=0 -)/' /etc/makepkg.conf
 		sudo sed -i 's/COMPRESSGZ=(gzip -c -f -n)/COMPRESSGZ=(pigz -c -f -n)/' /etc/makepkg.conf
-		sudo sed -i 's/COMPRESSBZ2=(bzip2 -c -f)/COMPRESSBZ2=(pbzip2 -c -f)/' /etc/makepkg.conf
+		sudo sed -i 's/COMPRESSBZ2=(bzip2 -c -f)/COMPRESSBZ2=(lbzip2 -c -f)/' /etc/makepkg.conf
 		sudo sed -i 's/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q --threads=0 -)/' /etc/makepkg.conf
 	#pacman
 		sudo sed -i 's/#Color/Color/' /etc/pacman.conf
@@ -71,46 +60,36 @@ echo "Tuning system..."
 	 	sudo grub-mkconfig -o /boot/grub/grub.cfg	
 	#/etc/env
 	echo "MOZ_ENABLE_WAYLAND=1" | sudo tee -a /etc/environment
-	
-	clear
 
 echo "Installing AUR helper"
 	sleep 2s
 		cd
 		mkdir GitHub
 		#For paru
-		#sudo pacman -S --needed base-devel
-		# cd ~/GitHub && git clone https://aur.archlinux.org/paru.git && cd paru
-		# makepkg -si
-		#sudo sed -i "/BottomUp/s/^#//g" /etc/paru.conf
+		sudo pacman -S --needed base-devel
+		cd ~/GitHub && git clone https://aur.archlinux.org/paru.git && cd paru
+		makepkg -si
+		sudo sed -i "/BottomUp/s/^#//g" /etc/paru.conf
+		sudo sed -i "/CombinedUpgrade/s/^#//g" /etc/paru.conf
 		
 		#For yay
-		sudo pacman -S --needed base-devel
-		cd ~/GitHub && git clone https://aur.archlinux.org/yay.git && cd yay
-		makepkg -si
-		sleep 1s
-		yay -Y --devel --combinedupgrade --batchinstall --save
-
-	clear	
+		# sudo pacman -S --needed base-devel
+		# cd ~/GitHub && git clone https://aur.archlinux.org/yay.git && cd yay
+		# makepkg -si
+		# sleep 1s
+		# yay -Y --devel --combinedupgrade --batchinstall --save
 
 echo "Installing AUR pkg..."
 	sleep 2s
-		yay -S spotify minecraft-launcher zoom gnome-shell-extension-pop-shell-git marktext-bin \
-		onlyoffice papirus-folders-git whatsdesk-bin bash-pipes noisetorch gnome-browser-connector-git \
-		visual-studio-code-bin google-chrome catppuccin-gtk-theme-mocha catppuccin-gtk-theme-latte \
-		extension-manager
+		paru -S prismlauncher zoom gnome-shell-extension-pop-shell-git papirus-folders-git visual-studio-code-bin google-chrome extension-manager github-desktop-bin
 		
 		sleep 1s 
 		papirus-folders -C pink
-
-	clear
 
 echo "Systemd Service"
 	sleep 2s
 		systemctl enable ufw.service
 		systemctl enable power-profiles-daemon.service
-		
-	clear
 
 echo "Tune Electron App"
 	sleep 2s
@@ -120,16 +99,12 @@ echo "Tune Electron App"
 		sudo cp ~/GitHub/ArchStuff/marktext.desktop usr/share/applications/marktext.desktop
 		sudo cp ~/GitHub/ArchStuff/marktext.desktop usr/share/applications/heroic.desktop
 
-	clear
-
 echo "Huawei Settings"
 	sleep 2s
-		yay -S matebook-applet huawei-wmi
+		paru -S matebook-applet huawei-wmi
 		systemctl enable huawei-wmi-reinstate.service
 		systemctl enable huawei-wmi-privilege.service
 		sudo usermod -a -G huawei-wmi $USER
-	
-	clear
 
 echo "Copy Dots"
 	sleep 2s
